ada02a35cc5a08ac5056063c3515d44ed18b22f0
albertLuth <albert.masip.vela@est.fib.upc.edu>
2018-04-16 16:17:59 +0200

    pagina principal només mostra posts amb url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.where.not(url: '').order('points ASC')
   end
 
   # GET /newest
