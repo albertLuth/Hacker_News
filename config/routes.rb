d138232366a4f1e3c3a42be421dc8b681bdf1f0d
albertLuth <albert.masip.vela@est.fib.upc.edu>
2018-04-17 18:07:30 +0200

    ADD login/registre amb Google API

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
   get '/newest', to: "posts#newest"
   get '/ask', to: "posts#ask"
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :posts
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
