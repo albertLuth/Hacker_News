6e623659bfb03d9fa466bd325dd6b3d9035adab8
manelfg24 <manelfg24@gmail.com>
2018-04-23 10:38:48 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,15 +23,14 @@ ActiveRecord::Schema.define(version: 20180421173204) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
     t.string "title"
     t.string "url"
     t.text "text"
     t.integer "points"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "replies", force: :cascade do |t|
