a4a0f062c91a628a1a2f9bddc89885c67d3d73ff
sergi95 <sergi.casas.monserrate@est.fib.upc.edu>
2018-05-02 21:46:37 +0200

    edits ok

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -43,7 +43,7 @@ class RepliesController < ApplicationController
   def update
     respond_to do |format|
       if @reply.update(reply_params)
-        format.html { redirect_to @reply, notice: 'Reply was successfully updated.' }
+        format.html { redirect_to @reply.comment.post, notice: 'Reply was successfully updated.' }
         format.json { render :show, status: :ok, location: @reply }
       else
         format.html { render :edit }
