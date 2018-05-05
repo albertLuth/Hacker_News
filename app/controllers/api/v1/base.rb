c083e9db6c3815ace6a5703e5278e49c5471dbd6
albertLuth <albert.masip.vela@est.fib.upc.edu>
2018-05-05 13:26:47 +0200

    API users basic

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -2,7 +2,7 @@ module API
     module V1
       class Base < Grape::API
         mount API::V1::Posts
-        # mount API::V1::AnotherResource
+        mount API::V1::Users
       end
     end
   end  
