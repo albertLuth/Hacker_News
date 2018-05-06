d730c5a48c54271ed26d936a9c0673ac57fcc82a
albertLuth <albert.masip.vela@est.fib.upc.edu>
2018-05-06 16:30:30 +0200

    edit i delete posts

--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -15,24 +15,56 @@ module API
           params do
             requires :id, type: String, desc: "ID of the post"
           end
-          get ":id", root: "post" do
+          get ":id" do
             Post.where(id: permitted_params[:id]).first!
           end
 
           desc "Create a new post"
           params do
             requires :title, type: String
-            requires :url, type: String
-            requires :text, type: String 
           end
           post "add" do
-            token = request.headers["Token"]
+            token = request.headers["Authentication"]
             user = User.where(uid: token).first
             if user != nil
               @post = Post.new(params)
               @post.user_id = user.id
               @post.user_name = user.name
               @post.points = 0
+              @post.save
+            else 
+              error!('Unauthorized.', 401)
+            end
+          end
+
+          desc "Delete a post"
+          params do
+            requires :id, type: String, desc: 'ID of the post'
+          end
+          delete ":id" do
+            token = request.headers["Authentication"]
+            @user = User.where(uid: token).first
+            @post = Post.where(id: permitted_params[:id]).first!
+            if @user && @user.id == @post.user_id
+              @post.destroy
+            else 
+              error!('Unauthorized.', 401)
+            end
+          end
+
+          desc "Edit a post"
+          params do
+            requires :id, type: String, desc: 'ID of the post'
+            requires :title, type: String
+          end
+          put ":id" do
+            token = request.headers["Authentication"]
+            @user = User.where(uid: token).first
+            @post = Post.where(id: permitted_params[:id]).first!
+            if @user && @user.id == @post.user_id
+              @post.title = params[:title]
+              @post.url = params[:url]
+              @post.text = params[:text]
               @post.save
             else 
               error!('Unauthorized.', 401)
