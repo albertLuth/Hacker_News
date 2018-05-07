df3fe0989914681cee200aa5d7501220187cc8c9
albertLuth <albert.masip.vela@est.fib.upc.edu>
2018-05-07 11:32:39 +0200

    bug solved, no es podia editar un usuari

--- app/controllers/api/v1/users.rb
+++ app/controllers/api/v1/users.rb
@@ -11,19 +11,19 @@ module API
           get ":id", root: "user" do
             User.where(id: permitted_params[:id]).first!
           end
-        end
 
-        desc "Edit a user"
-        put "" do
-          token = request.headers["Authentication"]
-          @user = User.where(uid: token).first
-          if @user
-            @user.name = params[:name] == nil ? @user.name : params[:name]
-            @user.email = params[:email] == nil ? @user.email : params[:email]
-            @user.about = params[:about] == nil ? @user.about :  params[:about]
-            @user.save
-          else 
-            error!('Unauthorized.', 401)
+          desc "Edit a user"
+          put "" do
+            token = request.headers["Authentication"]
+            @user = User.where(uid: token).first
+            if @user
+              @user.name = params[:name] == nil ? @user.name : params[:name]
+              @user.email = params[:email] == nil ? @user.email : params[:email]
+              @user.about = params[:about] == nil ? @user.about :  params[:about]
+              @user.save
+            else 
+              error!('Unauthorized.', 401)
+            end
           end
         end
       end
