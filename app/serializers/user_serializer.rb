ecb7eed0741ca5c63da4140e9325c6283bc8c608
kreaper97 <eric97lopez@gmail.com>
2018-05-13 17:18:06 +0200

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
+    attributes :id, :name, :email, :about, :created_at, :updated_at
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    attributes :id, :name, :email, :about, :created_at, :updated_at 
-=======
-    attributes :id, :name, :email, :about 
->>>>>>> 58b5e700dd0f26334790f2495659ae967483830f
-=======
-    attributes :id, :name, :email, :about 
->>>>>>> master
-  end  
+  end
